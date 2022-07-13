d6e9c87b5dd1800c13a670180068205c12520c88
wander <wander@ffactory.org>
2022-07-13 14:13:35 +0800

    Update pay.rs

--- src/pay.rs
+++ src/pay.rs
@@ -10,10 +10,18 @@ use gostd::builtin::byte;
 use gostd::net::http;
 use gostd::net::url;
 
-use crate::biz::{self, BizContenter};
+use crate::biz::{
+    self, BizContenter, TradeAppPayBiz, TradeCancelBiz, TradeCloseBiz, TradeCreateBiz,
+    TradeFastpayRefundQueryBiz, TradePageRefundBiz, TradePayBiz, TradePrecreateBiz, TradeQueryBiz,
+    TradeRefundBiz, TradeWapPayBiz,
+};
 use crate::request;
 use crate::request::{Request, Requester};
-use crate::response::{self, TradeCreateResponse};
+use crate::response::{
+    self, TradeCancelResponse, TradeCloseResponse, TradeCreateResponse,
+    TradeFastpayRefundQueryResponse, TradePageRefundResponse, TradePayResponse,
+    TradePrecreateResponse, TradeQueryResponse, TradeRefundResponse,
+};
 use crate::util::{build_form, json_get};
 pub trait Payer {
     fn get_api_url(&self) -> String;
@@ -31,8 +39,25 @@ pub trait Payer {
     fn set_sign_type(&mut self, sign_type: &str);
     fn set_charset(&mut self, charset: &str);
     fn execute(&self, request: &mut impl Requester) -> Result<Vec<byte>>;
-    fn do_alipay(&self, biz: &impl BizContenter) -> Result<Vec<byte>>;
-    fn trade_create(&self, biz: biz::TradeCreateBiz) -> Result<TradeCreateResponse>;
+    fn do_alipay(&self, biz_content: &impl BizContenter) -> Result<Vec<byte>>;
+    fn trade_create(&self, biz_content: &TradeCreateBiz) -> Result<TradeCreateResponse>;
+    fn trade_pay(&self, biz_content: &TradePayBiz) -> Result<TradePayResponse>;
+    fn trade_precreate(&self, biz_content: &TradePrecreateBiz) -> Result<TradePrecreateResponse>;
+    fn trade_app_pay(&self, biz_content: &TradeAppPayBiz) -> Result<String>;
+    fn trade_wap_pay(&self, biz_content: &TradeWapPayBiz) -> Result<String>;
+    fn trade_page_pay(&self, biz_content: &TradePayBiz) -> Result<String>;
+    fn trede_query(&self, biz_content: &TradeQueryBiz) -> Result<TradeQueryResponse>;
+    fn trade_cancel(&self, biz_content: &TradeCancelBiz) -> Result<TradeCancelResponse>;
+    fn trade_refund(&self, biz_content: &TradeRefundBiz) -> Result<TradeRefundResponse>;
+    fn trade_page_refund(
+        &self,
+        biz_content: &TradePageRefundBiz,
+    ) -> Result<TradePageRefundResponse>;
+    fn trade_fastpay_refund_query(
+        &self,
+        biz_content: &TradeFastpayRefundQueryBiz,
+    ) -> Result<TradeFastpayRefundQueryResponse>;
+    fn trade_close(&self, biz_content: &TradeCloseBiz) -> Result<TradeCloseResponse>;
 }
 
 #[derive(Debug, Default)]
@@ -190,10 +215,162 @@ impl Payer for PayClient {
         }
     }
 
-    fn trade_create(&self, biz: biz::TradeCreateBiz) -> Result<TradeCreateResponse> {
-        let mut res = TradeCreateResponse::default();
-        let body = self.do_alipay(&biz)?;
-        res = serde_json::from_slice(&body)?;
+    /// https://opendocs.alipay.com/apis/api_1/alipay.trade.create
+    ///
+    /// alipay.trade.create(统一收单交易创建接口)
+    ///
+    /// # Example
+    /// ```rust
+    /// use std::io::Result;
+    /// use alipay_sdk_rust::biz::{self, BizContenter};
+    /// use alipay_sdk_rust::pay::{PayClient, Payer};
+    /// use alipay_sdk_rust::response::TradeCreateResponse;
+    ///
+    /// fn main() -> Result<()> {
+    ///     let mut biz_content = biz::TradeCreateBiz::new();
+    ///     biz_content.set_subject("huawei Mate50");
+    ///     biz_content.set_out_trade_no("1620630871769533112");
+    ///     biz_content.set_total_amount("5");
+    ///     biz_content.set("seller_id", "2088621955702975");
+    ///     biz_content.set_buyer_id("2088102175953034");
+    ///     biz_content.set("Timestamp", "2022-07-11 16:09:04");///
+    ///
+    ///     let client = new_pay_client()?;
+    ///     let res = client.trade_create(biz_content)?;
+    ///     println!("{}", serde_json::to_string(&res)?);
+    ///     Ok(())
+    /// }
+    ///
+    /// fn new_pay_client() -> Result<impl Payer> {
+    ///     let client = PayClient::builder()
+    /// .api_url("https://openapi.alipaydev.com/gateway.do")
+    /// .app_id("2021000117650139")
+    /// .alipay_root_cert_sn("687b59193f3f462dd5336e5abf83c5d8_02941eef3187dddf3d3b83462e1dfcf6")
+    /// .alipay_public_key("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoEz4Tegg1Ytfn2N+jWzDTOBbUNhcpIXz7dV3K8yIsDzinPwYTzzZhlCLYlKbfJGpSDVU0K3I6TnyGfNPEg+R7EC/3ycY1nETh8mxQ4dgrz8irKlLxSRDHVtPg4luncDz2u0hMab9QJdqF8DXD5H+r0Pdt6CSJgKJqLa0awPV3/8cTQZbhZ4ontnRdcwWWcp4/TunkEc891Aa5FmzWp4hgBYcu3BGcazS1HQA4r6wTwRkiqKsCwZeVTag4CiOeqe/vRFTxTMKF4gjRzdhTapUfcBCmXblEA3i8/7OILEyNHceRAxxxIpUjTyoRJ4/2xd0kWbw1gmkLFM0Fzee0eVgoQIDAQAB")
+    /// .app_cert_sn("8c68b9753e5b9e0bb7704a981936ecce")
+    /// .charset_utf8()
+    /// .format_json()
+    /// .private_key("MIIEogIBAAKCAQEAog0N+rHllTO+e42Bc5mpvowolWVStyurL3Ou/86uRMN8im7WG1v44h09IaZpw4k6dpYEj89d7aLd7IwnBR5Wg84Ox2LMR/Y/Pzo10hjlvJJOk+igqepSTtB/4UX0cG/9tWceHAWOFuD8uw/SSJegC91a0MmLUBUpd4wWnN1iOSi0442iNvNk79Z6xLKIs4LNJGCNddxcofvpdqq4/5ywxHo24m5zPQf6/ttGk5jQQVrF+y2ckdHKd2h7ZSOYI7nzlzbZqK0UOMDuTvRs696fPa5wSEshE0RQBcn5iCltNTPyLsL3RGUUlsLOPsyT6cFZtUAKJ+J6wEqQxM5TrvNHywIDAQABAoIBAGDTuhGccFipVVzP3ZSsMV+4sZsqsrTd8+hjkCIrZbeSsvyoY2hvmRPKcreDjtiWS4eF9e3T8wTF9yKbT8lgKkORQQVkBDnPalUmO/hwhf0Z0rfQHQfKCiorrO129iqk0AyvM698pj0HbBt9xaE4cBoGxnfQpVxReLiEzRInucP6lhE79v1BwXCwMtRVvFPPIFaLJ02JGIywN+jnkpLwJj8TAu5u3JawlRnsFJgQeTdsHs4G6E11WBeo7OZtKPiKWMcj1nPU0Dnr+6VG89Rx/cxqlMrlTJBhKsLEzcVQwcc3M3UnMOU3Of7Mj1olnUGJ90apVukDFM5OI4Mfqi9etekCgYEA9ViJqVUdzJwqTK/gmbAsRvri9+rmWhfoqBMGeUoHktOGR/hMKJ/LrVa1oBIcVVNdLbI7Ks0kySGCa6qm/4YP2DCihj0GKwnHdPQ94pd3lWvUFZimNKi5V/+sREU0dKSqK3b7F0njtpR6zn+x8KpktO7izL3o8740KpSb7xGb0BUCgYEAqRaLShbDYjhSfvIzWAbuPNpvvtDWUNip1cuJwzTvDthECV5ltkhLGVWVnStch6OeTbK+llLDVw+j/YT1KetQcZ60tw2spn8nq6UvC2IFa2h61zpa8VWeRDfhyEIzoBE8DFAyeWjqYHyHJlh0BzRA2P3ts2LwwwhRa6OHhYzQ0F8CgYAUUvpMab2nNoSWh7dOY/a3Bo+IxA/DBNoEGldd8tD/y8AC9EGy19HykQ1Irldkhhxg7bPTDt1uP/Vi3+cnob5sRVMhVarOI+g++wCpZazFVwJhq5yRHi0EaiymFymKRB3IrfmM61UOyewGcTOXYTYoeuWU2mKS1n3RzS/BtS64JQKBgB2husVAGftzfVmL3l2V0VhOu3iIJpbCcXjrE3hnJWHHmpy9sztvjeGhsvd5Kt0GWm6pXWcAmAUA069RBpnTCCTxOCBAQDppXC1jZEwtYF/DTou7SUazx2mTFXk/yMZLXueVglLuhOxlxlV8+NBuYtLkJSzjsOes5H/lh5Fq7QknAoGAJ/LzTBLPy3terUgqejSxB4pr6PMbNd8wEStHN1RmR2v9Msuto7PUT7OOjQYIJwQLnxQUDr65bB5uR35v+L/rC6XUkzJM18YWvmOhFM8OsIYc4HdDhSmeFpMXdbd6entMJEX0bWrTbS/UdEcqE30kwuNuEFQ07LopGY1gBEe1G8U=")
+    /// .public_key("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAog0N+rHllTO+e42Bc5mpvowolWVStyurL3Ou/86uRMN8im7WG1v44h09IaZpw4k6dpYEj89d7aLd7IwnBR5Wg84Ox2LMR/Y/Pzo10hjlvJJOk+igqepSTtB/4UX0cG/9tWceHAWOFuD8uw/SSJegC91a0MmLUBUpd4wWnN1iOSi0442iNvNk79Z6xLKIs4LNJGCNddxcofvpdqq4/5ywxHo24m5zPQf6/ttGk5jQQVrF+y2ckdHKd2h7ZSOYI7nzlzbZqK0UOMDuTvRs696fPa5wSEshE0RQBcn5iCltNTPyLsL3RGUUlsLOPsyT6cFZtUAKJ+J6wEqQxM5TrvNHywIDAQAB")
+    /// .sign_type_rsa2()
+    /// .version_1_0()
+    /// .build()?;
+    ///     Ok(client)
+    /// }
+    /// ```
+    ///
+    /// # output
+    ///
+    /// `
+    /// {"response":{"code":"10000","msg":"Success","trade_no":"2022071322001453030502038801","out_trade_no":"1620630871769533112"},"sign":"NgDoQ8wIjV0MY3/hA3BDvHOz3Jw7y6CTdGPD+Q4GBsvrAHDDRwbdki+jSGr66zutKtklUYJsizKVIGbmJmKKBhVSNdxWCRI++keWfHjWDLjy59hiRix0l8oFh+dhnXaQqjAXEjqOxjtd6WGgO9FhgX1Kz6GAZ8NJobwzXKor8fotA0E5ztpcPcRRF4KmdVioofAdSf0o9UTpM24uFmGuBwi0Cfae70jctpmn0CMXJ36g2FEe3pcZIWm/KWDAXwGH6daQccULwVjUYN01OyeM93wKuLXJwEhvIeLpJeW4AiXpU21W/qNgYINPkjRA/h/HmG6ooG14VfdHNXPjuQ0/sg==","alipay_cert_sn":"28e0499cc4ef722406edd30274314430"}
+    /// `
+    fn trade_create(&self, biz_content: &TradeCreateBiz) -> Result<TradeCreateResponse> {
+        let body = self.do_alipay(biz_content)?;
+        let res: TradeCreateResponse = serde_json::from_slice(&body)?;
+        Ok(res)
+    }
+
+    /// https://opendocs.alipay.com/apis/api_1/alipay.trade.pay
+    ///
+    /// alipay.trade.pay(统一收单交易支付接口)
+    fn trade_pay(&self, biz_content: &TradePayBiz) -> Result<TradePayResponse> {
+        let body = self.do_alipay(biz_content)?;
+        let res: TradePayResponse = serde_json::from_slice(&body)?;
+        Ok(res)
+    }
+
+    /// https://opendocs.alipay.com/apis/api_1/alipay.trade.precreate
+    ///
+    /// alipay.trade.precreate(统一收单线下交易预创建)当面付-商家生成条码
+    fn trade_precreate(&self, biz_content: &TradePrecreateBiz) -> Result<TradePrecreateResponse> {
+        let body = self.do_alipay(biz_content)?;
+        let res: TradePrecreateResponse = serde_json::from_slice(&body)?;
+        Ok(res)
+    }
+
+    /// https://opendocs.alipay.com/apis/api_1/alipay.trade.app.pay
+    ///
+    /// alipay.trade.app.pay(app支付接口2.0)后端只生成form数据给前端调用
+    fn trade_app_pay(&self, biz_content: &TradeAppPayBiz) -> Result<String> {
+        let body = self.do_alipay(biz_content)?;
+        let res = String::from_utf8(body).map_err(|err| Error::new(ErrorKind::Other, err))?;
+        Ok(res)
+    }
+
+    /// https://opendocs.alipay.com/apis/api_1/alipay.trade.wap.pay
+    ///
+    /// alipay.trade.wap.pay(手机网站支付接口2.0)后端只生成form数据给前端调用
+    fn trade_wap_pay(&self, biz_content: &TradeWapPayBiz) -> Result<String> {
+        let body = self.do_alipay(biz_content)?;
+        let res = String::from_utf8(body).map_err(|err| Error::new(ErrorKind::Other, err))?;
+        Ok(res)
+    }
+
+    /// https://opendocs.alipay.com/apis/api_1/alipay.trade.page.pay
+    ///
+    /// alipay.trade.page.pay(统一收单下单并支付页面接口)后端只生成form数据给前端调用
+    fn trade_page_pay(&self, biz_content: &TradePayBiz) -> Result<String> {
+        let body = self.do_alipay(biz_content)?;
+        let res = String::from_utf8(body).map_err(|err| Error::new(ErrorKind::Other, err))?;
+        Ok(res)
+    }
+
+    /// Https://opendocs.alipay.com/apis/api_1/alipay.trade.query
+    ///
+    /// alipay.trade.query(统一收单线下交易查询)
+    fn trede_query(&self, biz_content: &TradeQueryBiz) -> Result<TradeQueryResponse> {
+        let body = self.do_alipay(biz_content)?;
+        let res: TradeQueryResponse = serde_json::from_slice(&body)?;
+        Ok(res)
+    }
+
+    ///  https://opendocs.alipay.com/apis/api_1/alipay.trade.cancel
+    ///
+    /// alipay.trade.cancel(统一收单交易撤销接口)
+    fn trade_cancel(&self, biz_content: &TradeCancelBiz) -> Result<TradeCancelResponse> {
+        let body = self.do_alipay(biz_content)?;
+        let res: TradeCancelResponse = serde_json::from_slice(&body)?;
+        Ok(res)
+    }
+
+    /// https://opendocs.alipay.com/apis/api_1/alipay.trade.refund
+    ///
+    /// alipay.trade.refund(统一收单交易退款接口)
+    fn trade_refund(&self, biz_content: &TradeRefundBiz) -> Result<TradeRefundResponse> {
+        let body = self.do_alipay(biz_content)?;
+        let res: TradeRefundResponse = serde_json::from_slice(&body)?;
+        Ok(res)
+    }
+
+    /// https://opendocs.alipay.com/apis/api_1/alipay.trade.page.refund
+    ///
+    /// alipay.trade.page.refund(统一收单退款页面接口)
+    fn trade_page_refund(
+        &self,
+        biz_content: &TradePageRefundBiz,
+    ) -> Result<TradePageRefundResponse> {
+        let body = self.do_alipay(biz_content)?;
+        let res: TradePageRefundResponse = serde_json::from_slice(&body)?;
+        Ok(res)
+    }
+    /// https://opendocs.alipay.com/apis/api_1/alipay.trade.fastpay.refund.query
+    ///
+    /// alipay.trade.fastpay.refund.query(统一收单交易退款查询)
+    fn trade_fastpay_refund_query(
+        &self,
+        biz_content: &TradeFastpayRefundQueryBiz,
+    ) -> Result<TradeFastpayRefundQueryResponse> {
+        let body = self.do_alipay(biz_content)?;
+        let res: TradeFastpayRefundQueryResponse = serde_json::from_slice(&body)?;
+        Ok(res)
+    }
+
+    /// https://opendocs.alipay.com/apis/api_1/alipay.trade.close
+    ///
+    /// alipay.trade.close(统一收单交易关闭接口)
+    fn trade_close(&self, biz_content: &TradeCloseBiz) -> Result<TradeCloseResponse> {
+        let body = self.do_alipay(biz_content)?;
+        let res: TradeCloseResponse = serde_json::from_slice(&body)?;
         Ok(res)
     }
 }
